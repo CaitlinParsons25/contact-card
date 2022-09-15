aa56986d5ff8f9389cc6cd818edeb4faa988d3fe
CaitlinParsons25 <103784858+CaitlinParsons25@users.noreply.github.com>
2022-09-15 14:32:01 -0500

    added IndexedDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -9,12 +9,16 @@ import "../css/index.css";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// import database
+import { initDb } from "./database";
+
 // import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function() {
+    initDb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
