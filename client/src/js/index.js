4702433b795a079e012cc0cbc63331b96beab502
CaitlinParsons25 <103784858+CaitlinParsons25@users.noreply.github.com>
2022-09-15 13:55:37 -0500

    added webpack and loaders

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,15 @@
+// import modules
 import "./form";
 import "./submit";
 
+// import css files
+import "../css/index.css";
+
+// import bootstrap
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
